2d8505f7e17a7dce3cefbfcf9acd61d4cc2c713a
kwingham <kyle_wingham@outlook.com>
2024-08-22 09:51:25 +0100

    updated

---
+++ week02/10-objects-intro-not-quick/app.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "Wigwam",
+  age: 37,
+  favouriteColour: "Teal",
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.favouriteColour);
+
+// 🎯 Create an object called car with a make, model, and colour. Create a string detailing the car's make, model, and colour for a website advert and log it to the console.
+// 🎯 Create an object called book with a title, author, and number of pages. Create a string explaining the book's details for a website advert and log it to the console.
